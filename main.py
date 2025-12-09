e08b5ab4315809416dcffa0b8e628a0043a10eda
austin <austin@RotorSVM.myguest.virtualbox.org>
2025-12-09 13:45:04 -0500

    added bit* as a ros package.

--- main.py
+++
@@ -1,8 +0,0 @@
-from bitstar.bit_star import Vertex, bit_star, Tree
-
-start = Vertex(5.0, 5.0)
-goal = Vertex(80.0, 80.0)
-tree = Tree(start, goal, radius=3.0)
-bit_star_planner = bit_star(tree, n_iters=100, sample_size=200)
-path = bit_star_planner.planning()
-print("Planned path:", path)
